37439c35f580fb8f0ab94269490ccdc1a6daf2db
Igor Bueno <igor.bueno@DBC-001272.local>
2023-08-07 16:36:09 -0400

    fix: status bar color

--- App.tsx
+++ App.tsx
@@ -1,9 +1,12 @@
+import { StatusBar } from "expo-status-bar";
 import People from "./src/presentation/screens/People";
 import { PeopleProvider } from "./src/presentation/store";
+import Theme from "./src/presentation/theme";
 
 export default function App() {
   return (
     <PeopleProvider>
+      <StatusBar backgroundColor={Theme.colors.white} />
       <People />
     </PeopleProvider>
   );
